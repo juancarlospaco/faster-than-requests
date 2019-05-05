3bcbc78daabace437b113dc446e097d9983c930a
Juan Carlos <juancarlospaco@gmail.com>
2019-05-05 17:31:45 -0300

    New features, proxy, timeout, etc

--- example/example.py
+++ example/example.py
@@ -1,22 +1,22 @@
 import faster_than_requests as requests
 
-print(requests.get("http://httpbin.org/get"))       # HTTP GET.
+print(requests.get("http://httpbin.org/get"))              # HTTP GET.
 
-print(requests.post("http://httpbin.org/post", """{"foo": "bar", "baz": true}"""))   # HTTP POST.
+print(requests.post("http://httpbin.org/post", """{"foo": "bar", "baz": true}"""))      # HTTP POST.
 
-print(requests.put("http://httpbin.org/put", """{"foo": "bar", "baz": true}"""))     # HTTP PUT.
+print(requests.put("http://httpbin.org/put", """{"foo": "bar", "baz": true}"""))        # HTTP PUT.
 
-print(requests.delete("http://httpbin.org/delete")) # HTTP DELETE.
+print(requests.delete("http://httpbin.org/delete"))        # HTTP DELETE.
 
-print(requests.patch("http://httpbin.org/patch", """{"foo": "bar", "baz": true}""")) # HTTP PATCH.
+print(requests.patch("http://httpbin.org/patch", """{"foo": "bar", "baz": true}"""))    # HTTP PATCH.
 
-print(requests.get2str("http://httpbin.org/get"))    # HTTP GET body only to string response.
+print(requests.get2str("http://httpbin.org/get"))          # HTTP GET body only to string response.
 
-print(requests.get2dict("http://httpbin.org/get"))   # HTTP GET body only to dictionary response.
+print(requests.get2dict("http://httpbin.org/get"))         # HTTP GET body only to dictionary response.
 
-print(requests.get2json("http://httpbin.org/get"))   # HTTP GET body only to JSON response.
+print(requests.get2json("http://httpbin.org/get"))         # HTTP GET body only to JSON response.
 
-print(requests.get2json_pretty("http://httpbin.org/get"))   # HTTP GET body only to Pretty-Printed JSON response.
+print(requests.get2json_pretty("http://httpbin.org/get"))  # HTTP GET body only to Pretty-Printed JSON response.
 
 print(requests.post2str("http://httpbin.org/post", """{"foo": "bar", "baz": true}"""))  # HTTP POST data only to string response.
 
@@ -33,11 +33,13 @@ print(requests.downloads("http://httpbin.org/image/jpeg", "foo.jpeg"))          
 print(requests.get2str_list(["http://httpbin.org/json", "http://httpbin.org/xml"]))     # HTTP GET body to string from a list.
 
 print(requests.get2ndjson_list(["http://httpbin.org/json",
-                                "http://httpbin.org/json"], "output.ndjson"))     # HTTP GET body to NDJSON file from a list.
+                                "http://httpbin.org/json"], "output.ndjson"))           # HTTP GET body to NDJSON file from a list.
 
 print(requests.downloads_list([("http://httpbin.org/image/jpeg", "foo.jpg"),            # HTTP GET Download a list of files.
                                ("http://httpbin.org/image/svg",  "bar.svg")]))
 
-print(requests.downloads_list_delay([("http://httpbin.org/image/jpeg", "foo.jpg"),            # HTTP GET Download a list of files.
+print(requests.downloads_list_delay([("http://httpbin.org/image/jpeg", "foo.jpg"),      # HTTP GET Download a list of files.
                                      ("http://httpbin.org/image/svg",  "bar.svg")],
                                     3))  # 3 Seconds Delay
+
+print(requests.setHeaders([("key", "value")]))             # Set HTTP Headers example.
