593b7c454fd0cbffb3d9fe07a9354588e3924d12
Juan Carlos <juancarlospaco@gmail.com>
2019-05-05 08:36:46 -0300

    Code style only

--- src/faster_than_requests.nim
+++ src/faster_than_requests.nim
@@ -33,35 +33,35 @@ import httpclient, json, tables, strutils, os, random, nimpy
 var client = newHttpClient(userAgent="")
 
 
-proc gets*(url: string): Table[string, string] {.inline, exportpy.} =
+proc gets*(url: string): Table[string, string] {. exportpy .} =
   ## HTTP GET an URL to dictionary.
   let r = client.get(url)
   {"body": r.body, "content-type": r.contentType, "status": r.status, "version": r.version,
    "content-length": $r.contentLength, "headers": replace($r.headers," @[", " [")}.toTable
 
 
-proc posts*(url, body: string): Table[string, string] {.inline, exportpy.} =
+proc posts*(url, body: string): Table[string, string] {. exportpy .} =
   ## HTTP POST an URL to dictionary.
   let r = client.post(url, body)
   {"body": r.body, "content-type": r.contentType, "status": r.status, "version": r.version,
    "content-length": $r.contentLength, "headers": replace($r.headers," @[", " [")}.toTable
 
 
-proc puts*(url, body: string): Table[string, string] {.inline, exportpy.} =
+proc puts*(url, body: string): Table[string, string] {. exportpy .} =
   ## HTTP PUT an URL to dictionary.
   let r = client.request(url, HttpPut, body)
   {"body": r.body, "content-type": r.contentType, "status": r.status, "version": r.version,
    "content-length": $r.contentLength, "headers": replace($r.headers," @[", " [")}.toTable
 
 
-proc patchs*(url, body: string): Table[string, string] {.inline, exportpy.} =
+proc patchs*(url, body: string): Table[string, string] {. exportpy .} =
   ## HTTP PATCH an URL to dictionary.
   let r = client.request(url, HttpPatch, body)
   {"body": r.body, "content-type": r.contentType, "status": r.status, "version": r.version,
    "content-length": $r.contentLength, "headers": replace($r.headers," @[", " [")}.toTable
 
 
-proc deletes*(url: string): Table[string, string] {.inline, exportpy.} =
+proc deletes*(url: string): Table[string, string] {. exportpy .} =
   ## HTTP DELETE an URL to dictionary.
   let r = client.request(url, HttpDelete)
   {"body": r.body, "content-type": r.contentType, "status": r.status, "version": r.version,
@@ -69,7 +69,7 @@ proc deletes*(url: string): Table[string, string] {.inline, exportpy.} =
 
 
 proc requests*(url, http_method, body: string, http_headers: openArray[tuple[key: string, val: string]],
-               debugs: bool = false): Table[string, string] {.inline, exportpy.} =
+               debugs: bool = false): Table[string, string] {. exportpy .} =
   ## HTTP requests low level function to dictionary.
   let headerss = newHttpHeaders(http_headers)
   if unlikely(debugs): echo url, "\n", http_method, "\n", body, "\n", headerss
@@ -81,24 +81,24 @@ proc requests*(url, http_method, body: string, http_headers: openArray[tuple[key
 # Extra HTTP Functions, go beyond the ones from requests.
 
 
-proc get2str*(url: string): string {.inline, exportpy.} =
+proc get2str*(url: string): string {. exportpy .} =
   ## HTTP GET body to string.
   client.getContent(url)
 
 
-proc getlist2list*(list_of_urls: openArray[string]): seq[seq[string]] {.inline, exportpy.} =
+proc getlist2list*(list_of_urls: openArray[string]): seq[seq[string]] {. exportpy .} =
   ## HTTP GET body from a list of urls to a list of lowercased strings (this is designed for quick web scrapping).
   for url in list_of_urls:
     result.add client.getContent(url).strip.toLowerAscii.splitlines
 
 
-proc get2str_list*(list_of_urls: openArray[string]): seq[string] {.inline, exportpy.} =
+proc get2str_list*(list_of_urls: openArray[string]): seq[string] {. exportpy .} =
   ## HTTP GET body to string from a list of URLs.
   for url in list_of_urls:
     result.add client.getContent(url)
 
 
-proc get2ndjson_list*(list_of_urls: openArray[string], ndjson_file_path: string) {.inline, discardable, exportpy.} =
+proc get2ndjson_list*(list_of_urls: openArray[string], ndjson_file_path: string) {. discardable, exportpy .} =
   ## HTTP GET body to NDJSON file from a list of URLs.
   var
     temp: string
@@ -110,71 +110,71 @@ proc get2ndjson_list*(list_of_urls: openArray[string], ndjson_file_path: string)
   ndjson.close()
 
 
-proc get2json*(url: string): string {.inline, exportpy.} =
+proc get2json*(url: string): string {. exportpy .} =
   ## HTTP GET body to JSON.
   result.toUgly client.getContent(url).parseJson
 
 
-proc get2json_pretty*(url: string): string {.inline, exportpy.} =
+proc get2json_pretty*(url: string): string {. exportpy .} =
   ## HTTP GET body to pretty-printed JSON.
   client.getContent(url).parseJson.pretty
 
 
-proc get2dict*(url: string): seq[Table[string, string]] {.inline, exportpy.} =
+proc get2dict*(url: string): seq[Table[string, string]] {. exportpy .} =
   ## HTTP GET body to dictionary.
   for i in client.getContent(url).parseJson.pairs:
     result.add {i[0]: i[1].pretty}.toTable
 
 
-proc get2assert*(url, expected: string) {.inline, discardable, exportpy.} =
+proc get2assert*(url, expected: string) {. discardable, exportpy .} =
   ## HTTP GET body to assert.
   doAssert client.getContent(url).strip == expected.strip
 
 
-proc post2str*(url, body: string): string {.inline, exportpy.} =
+proc post2str*(url, body: string): string {. exportpy .} =
   ## HTTP POST body to string.
   client.postContent(url, body)
 
 
-proc post2list*(url, body: string): seq[string] {.inline, exportpy.} =
+proc post2list*(url, body: string): seq[string] {. exportpy .} =
   ## HTTP POST body to list of lowercased strings (this is designed for quick web scrapping).
   client.postContent(url).strip.toLowerAscii.splitlines
 
 
-proc post2json*(url, body: string): string {.inline, exportpy.} =
+proc post2json*(url, body: string): string {. exportpy .} =
   ## HTTP POST body to JSON.
   result.toUgly client.postContent(url, body).parseJson
 
 
-proc post2json_pretty*(url, body: string): string {.inline, exportpy.} =
+proc post2json_pretty*(url, body: string): string {. exportpy .} =
   ## HTTP POST body to pretty-printed JSON.
   client.postContent(url, body).parseJson.pretty
 
 
-proc post2dict*(url, body: string): seq[Table[string, string]] {.inline, exportpy.} =
+proc post2dict*(url, body: string): seq[Table[string, string]] {. exportpy .} =
   ## HTTP POST body to dictionary.
   for i in client.postContent(url, body).parseJson.pairs:
     result.add {i[0]: i[1].pretty}.toTable
 
 
-proc post2assert*(url, body, expected: string) {.inline, discardable, exportpy.} =
+proc post2assert*(url, body, expected: string) {. discardable, exportpy .} =
   ## HTTP POST body to assert.
   doAssert client.postContent(url, body).strip == expected.strip
 
 
-proc downloads*(url, filename: string) {.inline, discardable, exportpy.} =
+proc downloads*(url, filename: string) {. discardable, exportpy .} =
   ## Download a file ASAP, from url, filename arguments.
   client.downloadFile(url, filename)
 
 
-proc downloads_list*(list_of_files: openArray[tuple[url: string, filename: string]]) {.inline, discardable, exportpy.} =
+proc downloads_list*(list_of_files: openArray[tuple[url: string, filename: string]]) {. discardable, exportpy .} =
   ## Download a list of files ASAP, like [(url, filename), (url, filename), ...]
   for item in list_of_files:
     client.downloadFile(item[0], item[1])
 
 
 proc downloads_list_delay*(list_of_files: openArray[tuple[url: string, filename: string]],
-                           delay: int, randoms: bool = false, debugs: bool = false) {.inline, discardable, exportpy.} =
+                           delay: int, randoms: bool = false, debugs: bool = false) {. discardable, exportpy .} =
   ## Download a list of files with delay, like [(url, filename), (url, filename), ...]
   var espera = delay * 1000
   if randoms: randomize()
