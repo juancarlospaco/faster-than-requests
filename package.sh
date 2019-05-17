80221e149f195160a962d6c6b2d74004b2dfa9ee
Juan Carlos <juancarlospaco@gmail.com>
2019-05-17 18:36:44 -0300

    Improve build

--- package.sh
+++ package.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 rm --verbose --force dist/*.zip
+
 python3 setup.py --verbose sdist --formats=zip
+
 rm --verbose --force --recursive faster_than_requests.egg-info/
+
 rm --verbose --force *.c
+
 rm --verbose --force nimbase.h
 
 
