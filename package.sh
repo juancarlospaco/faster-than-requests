4805e74ba7bca4c14a02053166ac77292f48d4d8
Juan Carlos <juancarlospaco@gmail.com>
2019-05-17 17:56:28 -0300

    Clean up

--- package.sh
+++ package.sh
@@ -4,3 +4,8 @@ python3 setup.py --verbose sdist --formats=zip
 rm --verbose --force --recursive faster_than_requests.egg-info/
 rm --verbose --force *.c
 rm --verbose --force nimbase.h
+
+
+# pip3 uninstall faster_than_requests --yes
+# pip3 install dist/faster_than_requests-0.2.zip --no-binary :all:
+# python -c "import faster_than_requests"
