5e1801d24fc86d480e03f6c69a6ab171562b0796
Juan Carlos <juancarlospaco@gmail.com>
2019-05-17 18:36:37 -0300

    Improve build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-nim compileToC -d:release -d:ssl --opt:speed --gc:markAndSweep --noMain --compileOnly --nimcache:. src/faster_than_requests.nim
+nim compileToC --compileOnly -d:release -d:ssl --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_requests.so src/faster_than_requests.nim
+
 cp --verbose --force ~/.choosenim/toolchains/nim-0.19.6/lib/nimbase.h nimbase.h
+
 rm --verbose --force faster_than_requests.json
