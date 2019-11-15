1e40af7ed7d69cc0d8e8b44e7aa2775e213c1727
juancarlospaco <juancarlospaco@gmail.com>
2019-11-14 21:08:51 -0300

    Multi-Threading

--- examples/example3.py
+++ examples/example3.py
@@ -2,10 +2,9 @@ import time
 import faster_than_requests
 
 
-check_url = "https://httpbin.org/get"
-lista = [(check_url, str(i) + ".json") for i in range(25)]
+lista = [("https://httpbin.org/get", str(i) + ".json") for i in range(25)]
 
 
 time2 = time.time()
 faster_than_requests.downloads_list(lista, threads = True)
-print(f'Faster_than_requests = {time.time() - time2}')
+print(time.time() - time2)
