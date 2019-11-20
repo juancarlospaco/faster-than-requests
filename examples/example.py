779a7c401eb70ec85b8815d4827dfb5ef442cb33
juancarlospaco <juancarlospaco@gmail.com>
2019-11-20 03:21:51 -0300

    Add Multi-Tag Ready-Made Web Scrapper from a list of URLs

--- examples/example.py
+++ examples/example.py
@@ -17,39 +17,28 @@ print(requests.get2dict("http://httpbin.org/get"))         # HTTP GET body only 
 
 print(requests.get2json("http://httpbin.org/get"))         # HTTP GET body only to JSON response.
 
-print(requests.get2json_pretty("http://httpbin.org/get"))  # HTTP GET body only to Pretty-Printed JSON response.
-
 print(requests.post2str("http://httpbin.org/post", """{"foo": "bar", "baz": true}"""))  # HTTP POST data only to string response.
 
 print(requests.post2dict("http://httpbin.org/post", """{"foo": "bar", "baz": true}""")) # HTTP POST data only to dictionary response.
 
 print(requests.post2json("http://httpbin.org/post", """{"foo": "bar", "baz": true}""")) # HTTP POST data to JSON response.
 
-print(requests.post2json_pretty("http://httpbin.org/post", """{"foo": "bar", "baz": true}""")) # HTTP POST data to Pretty-Printed JSON response.
-
 print(requests.requests("http://httpbin.org/get", "get", "", [("key", "value")]))       # HTTP GET/POST/PUT/DELETE/PATCH,Headers,etc.
 
 print(requests.requests2("http://httpbin.org/get", "get", "", [("key", "value")],
-                         userAgent="FasterThanRequests", timeout=9000, maxRedirects=9)) # HTTP GET/POST/PUT/DELETE/PATCH,Headers,etc.
+                          userAgent="FasterThanRequests", timeout=9000, maxRedirects=9)) # HTTP GET/POST/PUT/DELETE/PATCH,Headers,etc.
 
 print(requests.downloads("http://httpbin.org/image/jpeg", "foo.jpeg"))                  # HTTP GET Download 1 file.
 
-print(requests.get2str_list(["http://httpbin.org/json", "http://httpbin.org/xml"]))     # HTTP GET body to string from a list.
+print(requests.get2str2(["http://httpbin.org/json", "http://httpbin.org/xml"]))     # HTTP GET body to string from a list.
 
-print(requests.get2ndjson_list(["http://httpbin.org/json",
-                                "http://httpbin.org/json"], "output.ndjson"))           # HTTP GET body to NDJSON file from a list.
+print(requests.get2ndjson(["http://httpbin.org/json", "http://httpbin.org/json"], "output.ndjson")) # HTTP GET body to NDJSON file from a list.
 
-print(requests.downloads_list([("http://httpbin.org/image/jpeg", "foo.jpg"),            # HTTP GET Download a list of files.
-                               ("http://httpbin.org/image/svg",  "bar.svg")]))
-
-print(requests.downloads_list_delay([("http://httpbin.org/image/jpeg", "foo.jpg"),      # HTTP GET Download a list of files.
-                                     ("http://httpbin.org/image/svg",  "bar.svg")],
-                                    3))  # 3 Seconds Delay
+print(requests.downloads2([("http://httpbin.org/image/jpeg", "foo.jpg"),            # HTTP GET Download a list of files.
+                            ("http://httpbin.org/image/svg",  "bar.svg")]))
 
 requests.setHeaders([("key", "value")])                    # Set HTTP Headers example.
 
 requests.debugConfig()                                     # Debug the internal Configuration.
 
 print(requests.tuples2json([("key0", "value0"), ("key1", "value1")]))
-
-print(requests.tuples2json_pretty([("key0", "value0"), ("key1", "value1")]))
