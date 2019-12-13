15755c72cb2c3cc99e73c7b9e5394feb3cf1211b
juancarlospaco <juancarlospaco@gmail.com>
2019-12-13 03:00:39 -0300

    Add Web Scraper 3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nim compileToC --compileOnly -d:release -d:ssl --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_requests.so src/faster_than_requests.nim
 
-cp --verbose --force ~/.choosenim/toolchains/nim-1.0.2/lib/nimbase.h nimbase.h
+cp --verbose --force ~/.choosenim/toolchains/nim-1.0.4/lib/nimbase.h nimbase.h
 
 rm --verbose --force faster_than_requests.json
