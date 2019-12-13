1cadb3956bbf6bd672805a0f749c67574c34f20d
juancarlospaco <juancarlospaco@gmail.com>
2019-12-13 01:23:04 -0300

    Add Web Scraper 3

--- examples/multitag_web_scrapper.py
+++
@@ -1,2 +0,0 @@
-from faster_than_requests import scrapper2
-print(scrapper2(["https://nim-lang.org", "https://nim-lang.org"], list_of_tags=["h1", "a"], case_insensitive=False, deduplicate_urls=False))
