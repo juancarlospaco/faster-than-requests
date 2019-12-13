033de00e175bcef96128c4b8e4b7a13063497c75
juancarlospaco <juancarlospaco@gmail.com>
2019-12-13 01:23:09 -0300

    Add Web Scraper 3

--- examples/multithread_web_scrapper.py
+++
@@ -1,2 +0,0 @@
-from faster_than_requests import scrapper
-print(scrapper(["https://nim-lang.org", "https://nim-lang.org"], html_tag="h1", case_insensitive=False, deduplicate_urls=False, threads=False))
