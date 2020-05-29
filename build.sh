7593c353249077877417a986f4d10681cf856bcc
Juan Carlos <JuanCarlospaco@gmail.com>
2020-05-29 16:00:24 -0300

    Update for better user experiences

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nim compileToC --compileOnly -f -d:release -d:danger -d:ssl --threads:on --app:lib --opt:speed -d:noSignalHandler --listFullPaths:off --excessiveStackTrace:off --tlsEmulation:off --exceptions:goto --gc:markAndSweep --nimcache:. --out:faster_than_requests.so src/faster_than_requests.nim
 
-cp --verbose --force ~/.choosenim/toolchains/nim-1.0.6/lib/nimbase.h nimbase.h
+cp --verbose --force ~/.choosenim/toolchains/nim-1.2.0/lib/nimbase.h nimbase.h
 
 rm --verbose --force faster_than_requests.json
