0730fdd9cdb8130b649cbe53b2ea01308785c2fb
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-20 16:19:50 -0300

    Documentation

--- src/gctricks.py
+++ src/gctricks.py
@@ -1,4 +1,4 @@
-import gc, atexit
+import gc, atexit, faster_than_requests
 
 class optimizeGC:
   """https://instagram-engineering.com/dismissing-python-garbage-collection-at-instagram-4dca40b29172"""
@@ -6,7 +6,9 @@ class optimizeGC:
   def __enter__(self):
     # gc.set_threshold(0)
     gc.disable()
+    faster_than_requests.init_client()
 
   def __exit__(self, etype, value, traceback):
     # atexit.register(os._exit, 0)
+    faster_than_requests.close_client()
     gc.enable()
