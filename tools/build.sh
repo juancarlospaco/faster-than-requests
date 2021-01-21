7a6cc924541bd18cbadb133b4550496639e3d36e
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-20 22:21:38 -0300

    Clean out old tools folder

--- tools/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-nim compileToC --compileOnly -f -d:release -d:danger -d:ssl --threads:on --app:lib --opt:speed -d:noSignalHandler --listFullPaths:off --excessiveStackTrace:off --tlsEmulation:off --exceptions:goto --gc:markAndSweep --nimcache:. --out:faster_than_requests.so src/faster_than_requests.nim
-
-cp --verbose --force ~/.choosenim/toolchains/nim-1.2.0/lib/nimbase.h nimbase.h
-
-rm --verbose --force faster_than_requests.json
