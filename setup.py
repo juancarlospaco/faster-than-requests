51cecc539494457614163b98b3d4393b84359e9a
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-09 17:57:02 -0300

    Add name and version

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools, nimporter
 
 setuptools.setup(
+  name = "faster_than_requests",
+  version = "0.0.1",
   install_requires = ["nimporter"],
   ext_modules      = nimporter.build_nim_extensions(danger = True),
 )
