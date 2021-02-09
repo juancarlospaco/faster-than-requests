d255e945e70c7345e9a796ca277533ab083de60a
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-09 00:26:24 -0300

    testing only

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools, nimporter
 
 setuptools.setup(
-  install_requires     = ["nimporter"],
-  ext_modules          = nimporter.build_nim_extensions(danger = True),
+  install_requires = ["nimporter"],
+  ext_modules      = nimporter.build_nim_extensions(danger = True),
 )
