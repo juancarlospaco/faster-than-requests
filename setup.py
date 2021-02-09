6259df1d0e112f069f97ab81948265bf7f8cb3ed
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-08 22:12:16 -0300

    testing only

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ assert platform.architecture()[0] == "64bit", "ERROR: Python must be 64 Bit!. OS
 assert sys.version_info > (3, 6, 0), "ERROR: Python version must be > 3.5!."
 
 setuptools.setup(
-  package_data         = {"": ["*.nim", "*.cfg"]},
+  package_data         = {"": ["*.nim"]},
   include_package_data = True,
   install_requires     = ["nimporter"],
   ext_modules          = nimporter.build_nim_extensions(danger = True),
