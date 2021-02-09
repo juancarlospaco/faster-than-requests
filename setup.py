210d29764665066e9d0d4c7ebcc198032bbac15f
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-08 21:33:46 -0300

    Refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import sys, platform, setuptools, nimporter
 
 assert platform.architecture()[0] == "64bit", "ERROR: Python must be 64 Bit!. OS must be 64 Bit!."
 assert sys.version_info > (3, 6, 0), "ERROR: Python version must be > 3.5!."
-# atexit.register(lambda: print(importlib.metadata.distribution(package_name).files))
 
 setuptools.setup(
   package_data         = {"": ["*.nim"]},
