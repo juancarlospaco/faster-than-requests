bd940f2298f9f13f8602f77edcfc89435acd143e
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-10 00:00:04 -0300

    Fix

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import setuptools, nimporter
 
 setuptools.setup(
-  name = "faster_than_requests",
-  version = "0.0.1",
-  install_requires = ["nimporter"],
-  ext_modules      = nimporter.build_nim_extensions(danger = True),
+  name                 = "faster_than_requests",
+  version              = "0.0.1",
+  include_package_data = True,
+  install_requires     = ["nimporter"],
+  ext_modules          = nimporter.build_nim_extensions(danger = True),
 )
