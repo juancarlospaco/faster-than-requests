f1f91c89a824fe408aaf6f3fa6abef29821c2fb4
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-18 02:32:08 -0300

    testing testing

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
 
 setuptools.setup(
+  name                 = "faster_than_requests",
+  version              = "9.9.9",
   package_data         = {'': ['*.nim']},
   include_package_data = True,
 )
