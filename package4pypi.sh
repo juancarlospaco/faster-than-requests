0921db47051ebc535d02bbf3f6b101a6a837d9bd
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-24 16:05:24 -0300

    Clean out more

--- package4pypi.sh
+++ package4pypi.sh
@@ -1,3 +1,4 @@
 rm --verbose --force --recursive dist/faster_than_requests/faster_than_requests/
+rm --verbose --force --recursive dist/faster_than_requests/faster_than_requests/__pycache__/
 cp --verbose --recursive faster_than_requests/ dist/faster_than_requests/
 cd dist && zip -9 -T -v -r faster_than_requests.zip *
